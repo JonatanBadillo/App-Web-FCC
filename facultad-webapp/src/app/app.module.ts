97f04aac34e84e60ec8544b7a3305373b7795d46
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-01-18 11:24:59 -0600

    New Component registro-usuario-screen

--- facultad-webapp/src/app/app.module.ts
+++ facultad-webapp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { RegistroUsuarioScreenComponent } from './screens/registro-usuario-screen/registro-usuario-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginScreenComponent
+    LoginScreenComponent,
+    RegistroUsuarioScreenComponent
   ],
   imports: [
     BrowserModule,
