a72f38846437bc665dd2a745aa7cfe574913fcdb
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-02-08 19:18:32 -0600

    Agregados formularios de registro para administrador, alumnos y maestros

--- computacion-webapp/src/app/app.module.ts
+++ computacion-webapp/src/app/app.module.ts
@@ -8,13 +8,19 @@ import { LoginScreenComponent } from './screens/login-screen/login-screen.compon
 import { RegistroScreenComponent } from './screens/registro-screen/registro-screen.component';
 import { NavbarComponent } from './partials/navbar/navbar.component';
 import { MatRadioModule } from '@angular/material/radio';
+import { RegistroAdminComponent } from './partials/registro-admin/registro-admin.component';
+import { RegistroAlumnosComponent } from './partials/registro-alumnos/registro-alumnos.component';
+import { RegistroMaestrosComponent } from './partials/registro-maestros/registro-maestros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginScreenComponent,
     RegistroScreenComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegistroAdminComponent,
+    RegistroAlumnosComponent,
+    RegistroMaestrosComponent
   ],
   imports: [
     BrowserModule,
