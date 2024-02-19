c0a8bcbcec48d3f546ac48bbc1b8c587b3fd1ed0
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-02-19 09:46:54 -0600

    Añadir funcionalidad de registro de alumnos

--- computacion-webapp/src/app/partials/registro-alumnos/registro-alumnos.component.ts
+++ computacion-webapp/src/app/partials/registro-alumnos/registro-alumnos.component.ts
@@ -1,10 +1,77 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AlumnosService } from 'src/app/services/alumnos.service';
+declare var $:any;
 
 @Component({
-  selector: 'app-registro-alumnos',
+  selector: 'app-registro-alumno',
   templateUrl: './registro-alumnos.component.html',
   styleUrls: ['./registro-alumnos.component.scss']
 })
-export class RegistroAlumnosComponent {
+export class RegistroalumnoComponent implements OnInit{
   @Input() rol:string = "";
+
+  public alumno:any = {};
+  public editar:boolean = false;
+  public errors:any = {};
+  //Para contraseñas
+  public hide_1: boolean = false;
+  public hide_2: boolean = false;
+  public inputType_1: string = 'password';
+  public inputType_2: string = 'password';
+
+
+  constructor(
+    private alumnosService: AlumnosService
+  ){}
+
+  ngOnInit(): void {
+    this.alumno = this.alumno.esquemaAlumno();
+    this.alumno.rol = this.rol;
+    console.log("alumno: ", this.alumno);
+  }
+
+  public regresar(){
+
+  }
+
+  public registrar(){
+    //Validar
+    this.errors = [];
+
+    this.errors = this.alumnosService.validarAlumno(this.alumno, this.editar);
+    if(!$.isEmptyObject(this.errors)){
+      return false;
+    }
+
+    // TODO:Después registraremos alumno
+  }
+
+  public actualizar(){
+
+  }
+
+  //Funciones para password
+  showPassword()
+  {
+    if(this.inputType_1 == 'password'){
+      this.inputType_1 = 'text';
+      this.hide_1 = true;
+    }
+    else{
+      this.inputType_1 = 'password';
+      this.hide_1 = false;
+    }
+  }
+
+  showPwdConfirmar()
+  {
+    if(this.inputType_2 == 'password'){
+      this.inputType_2 = 'text';
+      this.hide_2 = true;
+    }
+    else{
+      this.inputType_2 = 'password';
+      this.hide_2 = false;
+    }
+  }
 }
