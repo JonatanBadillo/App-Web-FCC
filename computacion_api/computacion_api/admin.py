47b827dc5cd12d5e7193371768025eeaf2564cbc
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-02-27 20:14:35 -0600

    Registrar modelo Alumnos en el administrador

--- computacion_api/computacion_api/admin.py
+++ computacion_api/computacion_api/admin.py
@@ -4,5 +4,5 @@ from computacion_api.models import *
         
  #Declaracion de los modelos del form
 admin.site.register(Administradores)
-# admin.site.register(Alumnos)
+admin.site.register(Alumnos)
 # admin.site.register(Maestros)
