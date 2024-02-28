1d9a65e84489976c115b5816ca5c09c93a287fc0
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-02-27 20:11:04 -0600

    Agregados modelos de Alumnos y Administradores en models.py

--- computacion_api/computacion_api/models.py
+++ computacion_api/computacion_api/models.py
@@ -22,3 +22,20 @@ class Administradores(models.Model):
 
     def __str__(self):
         return "Perfil del admin "+self.first_name+" "+self.last_name
+    
+
+class Alumnos(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, default=None)
+    matricula = models.CharField(max_length=255,null=True, blank=True)#matricula del alumno
+    fecha_nacimiento = models.DateTimeField(auto_now_add=False,null=True, blank=True)#fecha de nacimiento
+    curp = models.CharField(max_length=255,null=True, blank=True)#CURP
+    rfc = models.CharField(max_length=255,null=True, blank=True)#RFC
+    edad = models.IntegerField(null=True, blank=True)#edad
+    telefono = models.CharField(max_length=255, null=True, blank=True)#telefono
+    ocupacion = models.CharField(max_length=255,null=True, blank=True)#ocupacion
+    creation = models.DateTimeField(auto_now_add=True, null=True, blank=True)#fecha de creacion del registro
+    update = models.DateTimeField(null=True, blank=True)#fecha de actualizacion del registro
+
+    def __str__(self):
+        return "Perfil del alumno "+self.first_name+" "+self.last_name    
